6d48a461a8335c2b444a03c97d4f7227c7ae378b
Klunio <371608318@qq.com>
2019-05-20 15:54:32 +0800

    db connection read and write operation.

--- server/model/User.py
+++ server/model/User.py
@@ -1,5 +1,6 @@
 __all__ = ['BasicUser', 'AuthenticatedUser', 'Student', 'Company']
 
+
 class BasicUser(object):
     """
     Attributes:
@@ -7,12 +8,17 @@ class BasicUser(object):
         profile_photo: how to load the profile photo?
         nickname: user's nickname, the default value is wechat nickname
         phone_number: user's phone number
-        sexual: man or female
+        gender: man or female
         intro: introduction
         create_date: date when this user was created
     """
     __slots__ = ['wechat_id', 'nickname', 'phone_number', 'gender', 'profile_photo', 'intro', 'create_date', 'isprove']
-    def __init__(self, wechat_id, profile_photo, nickname, phone_number, gender, intro, create_date, isprove):
+
+    def __init__(self, wechat_id, profile_photo, nickname, phone_number, gender, intro, create_date, is_proved):
+        """
+
+        :type is_proved: Datetime
+        """
         self.wechat_id = wechat_id
         self.nickname = nickname
         self.phone_number = phone_number
@@ -20,7 +26,8 @@ class BasicUser(object):
         self.profile_photo = profile_photo
         self.intro = intro
         self.create_date = create_date
-        self.isprove = isprove
+        self.isprove = is_proved
+
 
 class AuthenticatedUser(BasicUser):
     """
@@ -28,28 +35,27 @@ class AuthenticatedUser(BasicUser):
         identification: the authentification that had been certified by the administrator
     """
     __slots__ = ['identification']
+
     def __init__(self):
         super(AuthenticatedUser, self).__init__()
         pass
 
-class Student(AuthenticatedUser):
 
+class Student(AuthenticatedUser):
     __slots__ = ['college', 'std_id', 'school', 'major']
+
     def __init__(self):
         super(Student, self).__init__()
         pass
 
+
 class Company(AuthenticatedUser):
     __slots__ = ['company']
+
     def __init__(self):
         super(Company, self).__init__()
         pass
 
 
-
-
-
-
 if __name__ == '__main__':
     b = BasicUser()
-

f21b35e02bd7089582311ba5a473e0dbc6c32df9
afterQingming <215155757@qq.com>
2019-06-25 22:34:26 +0800

    -

--- c_server.py
+++ c_server.py
@@ -1,6 +1,6 @@
 from certificationServer.routers import prove
 # prove.app.run(host='0.0.0.0', port=60006, threaded=True)
 
-from server.model.User import User
+# from server.model.User import User
 if __name__ == '__main__':
     prove.app.run(host='0.0.0.0', port=60006, threaded=True)
